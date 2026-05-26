90bd9094b81f892159f14ddb3e0b430ed066e71c
Upendra46 <165449655+Upendra46@users.noreply.github.com>
2026-05-26 13:43:35 +0530

    Update admin.py

--- backend/backend/ingestion/admin.py
+++ backend/backend/ingestion/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Tenant)
+admin.site.register(DataSource)
 admin.site.register(RawRecord)
 admin.site.register(NormalizedActivity)
+admin.site.register(ReviewDecision)
+admin.site.register(AuditLog)
